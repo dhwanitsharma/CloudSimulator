b9e214bd0dc77cbbc9b97859cbd081f5e71d7fad
Dhwanit Sharma <54510578+dhwanitsharma@users.noreply.github.com>
2022-11-27 16:51:10 -0600

    Change Simulation.scala

--- src/main/scala/Simulation.scala
+++ src/main/scala/Simulation.scala
@@ -6,10 +6,19 @@ import org.slf4j.LoggerFactory
 object Simulation:
   val logger = CreateLogger(classOf[Simulation])
 
-  @main def runSimulation =
+  def main(args: Array[String]): Unit = {
+    val inputSelection= args(0).toInt
     logger.info("Constructing a cloud model...")
-    //BasicCloudSimPlusExample.Start()
-    Iaas.Start()
+    inputSelection match {
+      case 1 => Experiment1.Start()
+      case 2 => Experiment2.Start()
+      case 3 => Experiment3.Start()
+      case 4 => Saas.Start()
+      case 5 => Paas.Start()
+      case 6 => Iaas.Start()
+      case default => println("Please select the correct choice of input from 1 to 6")
+    }
     logger.info("Finished cloud simulation...")
+  }
 
 class Simulation
